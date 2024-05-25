6366b05d35c64645953e123e83add50a6152bb32
Bence Kormos <1123242+benqus@users.noreply.github.com>
2024-05-25 08:58:25 +0200

    le commit

---
+++ src/a.ts
@@ -0,0 +1,8 @@
+export function main() {
+  // Object.entries(process.env).forEach(([k, v]: [ string, string? ]): void => {
+  //   if (k.startsWith('npm_package_config')) {
+  //     console.log(k, v);
+  //   }
+  // });
+  return 'Hello A!';
+}
